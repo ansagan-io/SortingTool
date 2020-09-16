69c2074527d1cd8ede0ae772d8095bdaac6de203
ansagan-io <islamgaliev@mail.ru>
2020-09-16 16:30:00 +0500

    Stage 6/6 was successfully completed. New features: 1. Now an user can read data from file and write results to the file

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -1,77 +1,104 @@
 package sorting;
 
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(final String[] args) {
-        int sortIndex = -1;
-        int dataIndex = -1;
+        String dataType = "word";
+        String sortType = "natural";
+        String inputFile = null;
+        String outputFile = null;
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-dataType")) {
-                dataIndex = i;
+                try {
+                    dataType = args[i + 1];
+                    i++;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No data type defined!");
+                    System.exit(0);
+                }
             }
+
             if (args[i].equals("-sortingType")) {
-                sortIndex = i;
+                try {
+                    sortType = args[i + 1];
+                    i++;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No sorting type defined!");
+                    System.exit(0);
+                }
+            }
+
+            if (args[i].equals("-inputFile")) {
+                try {
+                    inputFile = args[i + 1];
+                    i++;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No input file defined!");
+                    System.exit(0);
+                }
+            }
+            if (args[i].equals("-outputFile")) {
+                try {
+                    outputFile = args[i + 1];
+                    i++;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("No output file defined!");
+                    System.exit(0);
+                }
+            }
+
+            if (args[i].startsWith("-")) {
+                System.out.println("\"" + args[i] + "\" isn't a valid parameter. It's skipped");
             }
         }
-        for (int i = 4; i < args.length; i++) {
-            System.out.println("\""  + args[i] + "\" isn't a valid parameter. It's skipped");
-        }
 
-        String dataType = null;
-        String sortType = null;
-
-        try {
-            if (dataIndex == -1) {
-                dataType = "word";
-            } else
-                dataType = args[dataIndex + 1];
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("No data type defined!");
-            System.exit(0);
-        }
-
-        try {
-            if (sortIndex == -1) {
-                sortType = "natural";
-            } else {
-                sortType = args[sortIndex + 1];
-            }
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("No sorting type defined!");
-            System.exit(0);
-        }
-
-        switch (dataType) {
+        switch (Objects.requireNonNull(dataType)) {
             case "long":
-                sortLongs(sortType);
+                sortLongs(sortType, inputFile, outputFile);
                 break;
             case "line":
-                sortLines(sortType);
+                sortLines(sortType, inputFile, outputFile);
                 break;
             case "word":
-                sortWords(sortType);
+                sortWords(sortType, inputFile, outputFile);
                 break;
         }
-
     }
 
-    private static void sortLines(String sortType) {
+    private static void sortLines(String sortType, String inputFile, String outputFile) {
         ArrayList<String> sortedDataEntries = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            sortedDataEntries.add(scanner.nextLine());
+        StringBuilder output = new StringBuilder();
+
+        if (inputFile != null) {
+            File file = new File(inputFile);
+            try (Scanner fileScan = new Scanner(file)) {
+                while (fileScan.hasNextLine()) {
+                    sortedDataEntries.add(fileScan.nextLine());
+                }
+            } catch (IOException e) {
+                System.out.println("Some error occurred: " + e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            while (scanner.hasNextLine()) {
+                sortedDataEntries.add(scanner.nextLine());
+            }
         }
-        System.out.println("Total lines: " + sortedDataEntries.size());
+
+        output.append("Total lines: ").append(sortedDataEntries.size()).append("\n");
         Collections.sort(sortedDataEntries);
 
         if (sortType.equals("natural")) {
 
-            System.out.print("Sorted data: \n");
+            output.append("Sorted data: \n");
             for (String line : sortedDataEntries) {
-                System.out.println(line);
+                output.append(line).append("\n");
             }
         } else {
             HashMap<String, Integer> dataEntryToCount = new HashMap<>();
@@ -87,31 +114,60 @@ public class Main {
                 } else {
                     countToDataEntries.put(pairs.getValue(), new TreeSet<>(Collections.singletonList(pairs.getKey())));
                 }
-
             }
 
             for (HashMap.Entry<Integer, TreeSet<String>> pairs : countToDataEntries.entrySet()) {
                 for (String line : pairs.getValue()) {
                     int percent = (100 / sortedDataEntries.size()) * pairs.getKey();
-                    System.out.printf("%s: %d time(s), %d%%\n", line, pairs.getKey(), percent);
+                    output.append(String.format("%s: %d time(s), %d%%\n", line, pairs.getKey(), percent));
                 }
             }
+
+
+        }
+
+        if (outputFile != null) {
+            File file = new File(outputFile);
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.append(output);
+            } catch (IOException e) {
+                System.out.println("Some error occurred: " + e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            System.out.println(output);
         }
     }
 
-    private static void sortWords(String sortType) {
+    private static void sortWords(String sortType, String inputFile, String outputFile) {
         ArrayList<String> sortedDataEntries = new ArrayList<>();
-        while (scanner.hasNext()) {
-            sortedDataEntries.add(scanner.next());
+        StringBuilder output = new StringBuilder();
+
+        if (inputFile != null) {
+            File file = new File(inputFile);
+            try (Scanner fileScan = new Scanner(file)) {
+                while (fileScan.hasNext()) {
+                    sortedDataEntries.add(fileScan.next());
+                }
+            } catch (IOException e) {
+                System.out.println("Some error occurred: " + e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            while (scanner.hasNext()) {
+                sortedDataEntries.add(scanner.next());
+            }
         }
-        System.out.println("Total words: " + sortedDataEntries.size());
+
+        output.append("Total words: ").append(sortedDataEntries.size()).append("\n");
+
         Collections.sort(sortedDataEntries);
 
         if (sortType.equals("natural")) {
 
-            System.out.print("Sorted data: ");
+            output.append("Sorted data: ");
             for (String word : sortedDataEntries) {
-                System.out.print(word + " ");
+                output.append(word).append(" ");
             }
         } else {
             Map<String, Integer> dataEntryToCount = new HashMap<>();
@@ -132,32 +188,65 @@ public class Main {
             for (Map.Entry<Integer, Set<String>> pairs : countToDataEntries.entrySet()) {
                 for (String word : pairs.getValue()) {
                     int percent = (100 / sortedDataEntries.size()) * pairs.getKey();
-                    System.out.printf("%s: %d time(s), %d%%\n", word, pairs.getKey(), percent);
+                    output.append(String.format("%s: %d time(s), %d%%\n", word, pairs.getKey(), percent));
                 }
+            }
+
+            if (outputFile != null) {
+                File file = new File(outputFile);
+                try (FileWriter fileWriter = new FileWriter(file)) {
+                    fileWriter.append(output);
+                } catch (IOException e) {
+                    System.out.println("Some error occurred: " + e.getMessage());
+                    System.exit(0);
+                }
+            } else {
+                System.out.println(output);
             }
         }
     }
 
-    private static void sortLongs(String sortType) {
+    private static void sortLongs(String sortType, String inputFile, String outputFile) {
         ArrayList<Long> sortedDataEntries = new ArrayList<>();
-        while (scanner.hasNext()) {
-            String beParsed = scanner.next();
-            try {
-                long number = Long.parseLong(beParsed);
-                sortedDataEntries.add(number);
-            } catch (NumberFormatException e) {
-                System.out.println("\""  + beParsed + "\" isn't a long. It's skipped");
-            }
+        StringBuilder output = new StringBuilder();
 
+        if (inputFile != null) {
+            File file = new File(inputFile);
+            try (Scanner fileScan = new Scanner(file)) {
+                while (fileScan.hasNext()) {
+                    String beParsed = fileScan.next();
+                    try {
+                        long number = Long.parseLong(beParsed);
+                        sortedDataEntries.add(number);
+                    } catch (NumberFormatException e) {
+                        System.out.println("\"" + beParsed + "\" isn't a long. It's skipped");
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Some error occurred: " + e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            while (scanner.hasNext()) {
+                String beParsed = scanner.next();
+                try {
+                    long number = Long.parseLong(beParsed);
+                    sortedDataEntries.add(number);
+                } catch (NumberFormatException e) {
+                    System.out.println("\"" + beParsed + "\" isn't a long. It's skipped");
+                }
+
+            }
         }
-        System.out.println("Total numbers: " + sortedDataEntries.size());
+
+        output.append("Total numbers: ").append(sortedDataEntries.size()).append("\n");
         Collections.sort(sortedDataEntries);
 
         if (sortType.equals("natural")) {
 
-            System.out.print("Sorted data: ");
+            output.append("Sorted data: ");
             for (Long ints : sortedDataEntries) {
-                System.out.print(ints + " ");
+                output.append(ints).append(" ");
             }
         } else {
             Map<Long, Integer> dataEntryToCount = new HashMap<>();
@@ -178,9 +267,21 @@ public class Main {
             for (Map.Entry<Integer, Set<Long>> pairs : countToDataEntries.entrySet()) {
                 for (Long number : pairs.getValue()) {
                     int percent = (100 / sortedDataEntries.size()) * pairs.getKey();
-                    System.out.printf("%d: %d time(s), %d%%\n", number, pairs.getKey(), percent);
+                    output.append(String.format("%d: %d time(s), %d%%\n", number, pairs.getKey(), percent));
                 }
             }
+        }
+
+        if (outputFile != null) {
+            File file = new File(outputFile);
+            try (FileWriter fileWriter = new FileWriter(file)) {
+                fileWriter.append(output);
+            } catch (IOException e) {
+                System.out.println("Some error occurred: " + e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            System.out.println(output);
         }
     }
 }
